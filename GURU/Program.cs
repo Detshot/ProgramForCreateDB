a31b57e7b6687986102786296e27fa4055ed7e07
Ded <dmitrosmatko@gmail.com>
2024-06-24 19:38:17 +0300

    Розділено на методи, зроблена видалення спец символів

--- GURU/Program.cs
+++ GURU/Program.cs
@@ -1,7 +1,16 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.ComponentModel;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Net.Http;
+using System.Threading;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
@@ -9,59 +18,73 @@ class Program
     {
         ExtractionDB extractionDB = new ExtractionDB();
 
-        string component, pathImage;
+        string component, path;
+
         //component = "div.prod-selrow[data-id='10000']"; // проци
-        //pathImage = "Image\\Processors";
-        //extractionDB.Parse(component, pathImage, pathImage + $"\\Characteristics.txt");
+        //path = "Image\\Processors";
+        //extractionDB.Parse(component, path);
 
         //component = "div.prod-selrow[data-id='14000']"; // метеринки
-        //pathImage = "Image\\Motherboards";
-        //extractionDB.Parse(component, pathImage, pathImage + $"\\Characteristics.txt");
+        //path = "Image\\Motherboards";
+        //extractionDB.Parse(component, path);
 
         //component = "div.prod-selrow[data-id='16400']"; //оперативка
-        //pathImage = "Image\\RAM";
-        //extractionDB.Parse(component, pathImage, pathImage + $"\\Characteristics.txt");
+        //path = "Image\\RAM";
+        //extractionDB.Parse(component, path);
 
-        component = "div.prod-selrow[data-id='20000']"; //видеокарта
-        pathImage = "Image\\VideoCards";
-        extractionDB.Parse(component, pathImage, pathImage + $"\\Characteristics.txt");
+        //component = "div.prod-selrow[data-id='20000']"; //видеокарта
+        //path = "Image\\VideoCards";
+        //extractionDB.Parse(component, path);
+
+        component = "div.prod-selrow[data-id='24000']"; //корпус
+        path = "Image\\Housing";
+        extractionDB.Parse(component, path);
 
         //component = "div.prod-selrow[data-id='24000']"; //корпус
-        //pathImage = "Image\\Housing";
-        //extractionDB.Parse(component, pathImage, pathImage + $"\\Characteristics.txt");
+        //path = "Image\\Housing1";
+        //extractionDB.Parse(component, path, 15, 684);
 
         //component = "div.prod-selrow[data-id='40000']"; //БП
-        //pathImage = "Image\\PowerSupplies";
-        //extractionDB.Parse(component, pathImage, pathImage + $"\\Characteristics.txt");
-
+        //path = "Image\\PowerSupplies";
+        //extractionDB.Parse(component, path);
     }
 }
 
 class ExtractionDB
 {
-    //string component, pathImage, nameTXT;
+    List<string> headerTextList = new List<string>();
+    List<string> tableHtmlList = new List<string>();
+    List<string> prices = new List<string>();
+    int j = 2;
+    string filePath, component, pathImage;
+    IWebDriver driver;
 
-    void ScrollElementIntoView(IWebDriver driver, IWebElement element)
+    int k = 0;
+
+    public ExtractionDB() { }
+
+    void OpenChromeDriver()
+    {
+        var chromeDriverService = ChromeDriverService.CreateDefaultService(@"C:\Users\Коля\source\repos\GURU\GURU\bin\Debug\net8.0");
+        driver = new ChromeDriver(chromeDriverService);
+    }
+
+    void ScrollElementIntoView(IWebElement element)
     {
         IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
         js.ExecuteScript("arguments[0].scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' }); window.scrollBy(0, -15);", element);
+        Thread.Sleep(180);
+    }
 
-    }
-    bool CanScrollDown(IWebDriver driver)
-    {
-        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-        long documentHeight = (long)js.ExecuteScript("return document.documentElement.scrollHeight;");
-        long windowHeight = (long)js.ExecuteScript("return window.innerHeight;");
-        long scrollY = (long)js.ExecuteScript("return window.scrollY;");
-        return scrollY + windowHeight < documentHeight;
-    }
     void DownloadImage(string imageUrl, string destinationPath)
     {
-        using (WebClient client = new WebClient())
+        using (HttpClient client = new HttpClient())
         {
-            client.DownloadFile(imageUrl, destinationPath);
+            byte[] fileBytes = client.GetByteArrayAsync(imageUrl).Result;
+            File.WriteAllBytes(destinationPath, fileBytes);
         }
     }
+
     bool IsElementInView(IWebDriver driver, IWebElement element)
     {
         IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
@@ -71,131 +94,160 @@ class ExtractionDB
         return elementTop >= 0 && elementTop <= windowHeight;
     }
 
-
-    public void Parse(string component, string pathImage, string filePath)
+    void CreateTXT()
     {
-        //може винести це в конструктор
-        var chromeDriverService = ChromeDriverService.CreateDefaultService(@"C:\Users\Коля\source\repos\GURU\GURU\bin\Debug\net8.0");
-        IWebDriver driver = new ChromeDriver(chromeDriverService);
+        using (StreamWriter sw = new StreamWriter(filePath))
+        {
+            for (int i = 0; i < headerTextList.Count; i++)
+            {
+                sw.WriteLine($"Заголовок: {headerTextList[i]}");
+                sw.WriteLine($"Цена: {prices[i]}");
+                sw.WriteLine($"Характеристики: {tableHtmlList[i]}");
+                sw.WriteLine(new string('-', 98));
+            }
+        }
+    }
 
+    string SanitizeFileName(string fileName)
+    {
+        string invalidChars = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+        foreach (char c in invalidChars)
+        {
+            fileName = fileName.Replace(c.ToString(), "");
+        }
+        return fileName;
+    }
+
+    void FindingAndDownloadingImage(IList<IWebElement> productElements, int i, string headerText)
+    {
+        IWebElement imgElement = productElements[i].FindElement(By.TagName("img"));
+        string imageUrl = imgElement.GetAttribute("src");
+
+        string sanitizedHeaderText = SanitizeFileName(headerText);
+        string destinationPath = Path.Combine(pathImage, $"{k}_{sanitizedHeaderText}.jpg");
+
+        DownloadImage(imageUrl, destinationPath);
+        k++;
+    }
+
+    void NextPages(int j)
+    {
+        IWebElement pageTwoElement = driver.FindElement(By.XPath($"//li[@page='{j}']"));
+        ScrollElementIntoView(pageTwoElement);
+        Thread.Sleep(600);
+        pageTwoElement.Click();
+        Thread.Sleep(500);
+    }
+
+    void GoToPage(int j)
+    {
+        for (int i = 2; i <= j; i++)
+            NextPages(i);
+        this.j = j + 1;
+    }
+
+    void ClickElement(string component)
+    {
+        IWebElement prodSelRowElement = driver.FindElement(By.CssSelector(component));
+        IWebElement svgElement = prodSelRowElement.FindElement(By.CssSelector("svg.plus"));
+
+        ScrollElementIntoView(svgElement);
+        svgElement.Click();
+    }
+
+    void ClosePopup()
+    {
+        IWebElement skipButton = driver.FindElement(By.XPath("//b[contains(@class, 'dg-btn dg-skip') and contains(text(), 'Пропустить')]"));
+        skipButton.Click();
+    }
+
+    int FindLastPages()
+    {
+        IWebElement pagination = driver.FindElement(By.CssSelector("ul.pgnation.pager-products"));
+
+        var pageItems = pagination.FindElements(By.TagName("li"));
+        var lastPageItem = pageItems.Last(item => item.GetAttribute("page") != null);
+        string lastPageNumber = lastPageItem.GetAttribute("page");
+
+        return int.Parse(lastPageNumber);
+    }
+
+    void ParsePages()
+    {
+        IWebElement headerElement, linkElement, tableElement;
+        string headerText, tableHtml;
+
+        int pages = FindLastPages();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.75);
+        for (; ; j++)
+        {
+            IList<IWebElement> productElements = driver.FindElements(By.CssSelector(".scrl-blu.cat-products > article[id^='pdtid']"));
+
+            for (int i = 0; i < productElements.Count; i++)
+            {
+                headerElement = productElements[i].FindElement(By.CssSelector("header a[target='_blank']"));
+                headerText = headerElement.Text;
+
+                if (string.IsNullOrEmpty(headerText))
+                    continue;
+
+                if (headerTextList.Contains(headerText))
+                    continue;
+
+                linkElement = productElements[i].FindElement(By.CssSelector("header > a"));
+
+                ScrollElementIntoView(linkElement);
+                if (i == 0)
+                    Thread.Sleep(1000);
+
+                try
+                {
+                    linkElement.Click();
+                    tableElement = productElements[i].FindElement(By.CssSelector(".ofr-main table"));
+                    tableHtml = tableElement.GetAttribute("innerHTML");
+                    linkElement.Click();
+
+                    tableHtmlList.Add(tableHtml);
+                }
+                catch (NoSuchElementException)
+                {
+                    continue;
+                }
+
+                headerTextList.Add($"{k}_{headerText}");
+                prices.Add(productElements[i].FindElement(By.CssSelector("b.val")).GetAttribute("innerHTML"));
+
+                FindingAndDownloadingImage(productElements, i, headerText);
+            }
+            if (j == pages + 1)
+                break;
+
+            NextPages(j);
+            Thread.Sleep(800);
+        }
+
+        CreateTXT();
+    }
+
+    public void Parse(string component, string pathImage)
+    {
+        this.pathImage = pathImage;
+        this.component = component;
+        this.filePath = Path.Combine(pathImage, "Characteristics.txt");
+
+        OpenChromeDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         try
         {
             driver.Navigate().GoToUrl("https://elmir.ua/configurator/");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            ClosePopup();
+            ClickElement(component);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
 
-            // Пропустить всплывающее окно
-            IWebElement skipButton = driver.FindElement(By.XPath("//b[contains(@class, 'dg-btn dg-skip') and contains(text(), 'Пропустить')]"));
-            skipButton.Click();
+            ParsePages();
 
-            //натискання на потрібний тип компонента
-            IWebElement prodSelRowElement = driver.FindElement(By.CssSelector(component));
-            IWebElement svgElement = prodSelRowElement.FindElement(By.CssSelector("svg.plus"));
-
-            ScrollElementIntoView(driver, svgElement);
-            Thread.Sleep(250);
-
-            svgElement.Click();
-
-            //знаходження останньої сторінки
-            IWebElement pagination = driver.FindElement(By.CssSelector("ul.pgnation.pager-products"));
-            var pageItems = pagination.FindElements(By.TagName("li"));
-            var lastPageItem = pageItems.Last(item => item.GetAttribute("page") != null);
-            string lastPageNumber = lastPageItem.GetAttribute("page");
-
-            int pages = int.Parse(lastPageNumber);
-            //pages++;
-
-            IWebElement headerElement, linkElement, tableElement, imgElement, priceElement;
-            string headerText, tableHtml, imageUrl, tempImageName;
-            int k = 0;
-            List<string> headerTextList = new List<string>();
-            List<string> tableHtmlList = new List<string>();
-            List<string> prices = new List<string>();
-
-            for (int j = 2; ; j++)
-            {
-                // Найти все элементы внутри контейнера scrl-blu cat-products
-                IList<IWebElement> productElements = driver.FindElements(By.CssSelector(".scrl-blu.cat-products > article[id^='pdtid']"));
-
-                for (int i = 0; i < productElements.Count; i++)
-                {
-                    // Получить заголовок (имя продукта)
-                    headerElement = productElements[i].FindElement(By.CssSelector("header a[target='_blank']"));
-                    headerText = headerElement.Text;
-                    //headerText = Regex.Replace($"//a[contains(text(),'{headerText}')]", @" \([^)]*\) ", "");
-                    headerText = $"//a[contains(text(),'{headerText}')]";
-
-                    if (headerText == "")
-                        continue;
-
-                    imgElement = productElements[i].FindElement(By.TagName("img"));
-                    imageUrl = imgElement.GetAttribute("src");
-
-                    //знаходження зображення та його завантаження указану папку
-                    tempImageName = headerText.Replace("/", "");
-                    DownloadImage(imageUrl, @$"{pathImage}\{k}_{tempImageName}.jpg");
-
-
-                    //знаходження компонента (процесора)
-                    // Перевірка наявності елемента в списку headerTextList
-                    if (headerTextList.Contains(headerText))
-                    {
-                        continue;
-                    }
-
-                    linkElement = productElements[i].FindElement(By.CssSelector("header > a"));
-                    headerTextList.Add($"{k}_headerText");
-                    k++;
-                    // прокрутка до нужного елемнта
-                    if (CanScrollDown(driver))
-                    {
-                        ScrollElementIntoView(driver, linkElement);
-                        Thread.Sleep(180);
-                    }
-                    //откритие характеристик
-                    linkElement.Click();
-
-                    tableElement = driver.FindElement(By.CssSelector(".ofr-main table"));
-                    tableHtml = tableElement.GetAttribute("innerHTML");
-                    tableHtmlList.Add(tableHtml);
-
-                    //отримання ціни
-                    priceElement = productElements[i].FindElement(By.CssSelector("b.val"));
-                    prices.Add(priceElement.GetAttribute("innerHTML"));
-
-                    //закритие характеристик
-                    linkElement.Click();
-                }
-                if (j == pages + 1)
-                {
-                    break;
-                }
-                IWebElement pageTwoElement = driver.FindElement(By.XPath($"//li[@page='{j}']"));
-                ScrollElementIntoView(driver, pageTwoElement);
-                Thread.Sleep(180);
-                pageTwoElement.Click();
-                Thread.Sleep(1000);
-            }
-
-            //for (int i = 0; i < headerTextList.Count; i++)
-            //{
-            //    Console.WriteLine($"Заголовок: {headerTextList[i]}");
-            //    Console.WriteLine($"Цена: {prices[i]}");
-            //    Console.WriteLine($"Характеристики: {tableHtmlList[i]}");
-
-            //    Console.WriteLine(new string('-', 100));
-            //}
-
-            using (StreamWriter sw = new StreamWriter(filePath))
-            {
-                for (int i = 0; i < headerTextList.Count; i++)
-                {
-                    sw.WriteLine($"Заголовок: {headerTextList[i]}");
-                    sw.WriteLine($"Цена: {prices[i]}");
-                    sw.WriteLine($"Характеристики: {tableHtmlList[i]}");
-                    sw.WriteLine(new string('-', 100));
-                }
-            }
+            Console.WriteLine("Программа успешно завершила работу");
+            Console.WriteLine(component);
         }
         catch (Exception ex)
         {
@@ -204,9 +256,39 @@ class ExtractionDB
         finally
         {
             driver.Quit();
-            Console.WriteLine("Програма успішно завеошила роботу");
-            Console.WriteLine(component);
+            CreateTXT();
         }
     }
 
+    public void Parse(string component, string pathImage, int page, int k)
+    {
+        this.pathImage = pathImage;
+        this.component = component;
+        this.filePath = Path.Combine(pathImage, "Characteristics.txt");
+        this.k = k;
+
+        OpenChromeDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        try
+        {
+            driver.Navigate().GoToUrl("https://elmir.ua/configurator/");
+            ClosePopup();
+            ClickElement(component);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            GoToPage(page);
+            ParsePages();
+            Console.WriteLine("Программа успешно завершила работу");
+            Console.WriteLine(component);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+        finally
+        {
+            driver.Quit();
+            CreateTXT();
+        }
+    }
 }
